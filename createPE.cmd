25d01ccaef438a97af2d80a7b08f5e2974622aa2
Marcus Pallinger <marcus@pallinger.org>
2018-12-01 14:02:08 +1100

    2.1 (08/11/2013)
    * Allow the user to create a USB disk isntead of an ISO

--- createPE.cmd
+++ createPE.cmd
@@ -1,29 +1,42 @@
 @echo off
 
-:: PEBuilder v2.0
-:: Designed for use with the Windows 8 Assessment and Deployment Kit, but may work
-:: with earlier versions.
-:: The ADK can be found at http://www.microsoft.com/en-us/download/details.aspx?id=30652
+:: PEBuilder v2.1
+:: Designed for use with the Windows 8 Assessment and Deployment Kit, i
+:: but may work with earlier versions.
+:: The ADK can be found at 
+:: http://www.microsoft.com/en-us/download/details.aspx?id=30652
 ::
-:: It assumes Symantec Ghost Solution Suite is installed in the default directory.
-:: It also assumes that the commandline Virus Scan is unzipped in the desktop
-:: If you have a licence, Command line virus scan can be downloaded from 
+:: It assumes Symantec Ghost Solution Suite is installed in the default 
+:: directory. It also assumes that the commandline Virus Scan is unzipped
+:: in the desktop. If you have a licence, Command line virus scan can be 
+:: downloaded from 
 :: http://www.mcafeeasap.com/downloads/CLS/vscl-w32-6.0.1-l.zip
 :: It will install the latest Mcafee SuperDat.
 ::
-:: Virus scan may say "mcscan32.dll has failed its integrity check" if you aren't
-:: connected to the network. Start scan with the /NC switch.
+:: Virus scan may say "mcscan32.dll has failed its integrity check" if you 
+:: aren't connected to the network. Start scan with the /NC switch.
 :: for mroe info: https://kc.mcafee.com/corporate/index?page=content&id=kb68314
 ::
 :: To run this script, Click Start, and type deployment. Right-click Deployment 
 :: and Imaging Tools Environment and then select Run as administrator. Then run 
 :: this script in that command window.
 ::
-:: Usage:
-:: createPE architecture
-:: Options:
-::   architecture must be either x86 or amd64. It selects for which architecture to build
-::   the ISO.
+:: If you want to create a USB drive, prepare it according to the instructions
+:: at:
+:: http://technet.microsoft.com/en-us/library/hh825045.aspx
+:: Windows may not allow you to create two partitions on a USB drive. If the 
+:: above instructions fail, try the following:
+:: diskpart
+:: list disk
+:: select <disk number>
+:: clean
+:: rem === Create the Windows PE partition. ===
+:: create partition primary
+:: format quick fs=fat32 label="Windows PE"
+:: assign letter=P
+:: active
+::
+:: To get usage information, run without paramaters
 ::
 :: Release History:
 :: 1.0
@@ -34,6 +47,8 @@
 :: * Allow creation of an image on an x64 system
 :: * Allow the cration of 32 or 64 bit image
 :: * Change to the current directory once we finish
+:: 2.1
+:: * Allow the user to create a USB disk isntead of an ISO
 ::
 :: Future work:
 :: Create a script on the PE image that will update the DAT files
@@ -42,6 +57,7 @@
 :: Allow for building of a UEFI 64 bit version
 :: Investigate WoW64 on the PE image
 :: Check for Ghost and Mcafee directories, and abort if they aren't there
+:: Make the script more modular
 
 :: Remmeber the current directory
 set InitialLocation=%CD%
@@ -84,27 +100,48 @@ set mcafee=yes
 set archFlag=x86
 goto endDestArchitecture
 
+:endDestArchitecture
+
+set usb=no
+
+if "%2"=="" goto nousb
+if not "%2"=="/usb" goto usage
+
+if "%3"=="" goto usage
+
+set usb=yes
+set drive=%3
+set buildType=/ufd
+
+goto endOptions
+
+:nousb
+set usb=no
+set buildType=/iso
+
 :usage
 
 echo Usage:
-echo createPE architecture
+echo createPE architecture [/usb drive]
 echo Options:
-echo   architecture must be either x86 or amd64. It selects for which architecture to build
-echo   the ISO.
+echo   architecture: must be either x86 or amd64. It selects for which 
+echo     architecture to build the ISO.
+echo   /usb: Create a USB stick rather than an iso image.
+echo   drive: The drive letter to use when creating a USB disk 
 
 goto end
 
-
-:endDestArchitecture
-
+:endOptions
 
 set pedir=c:\winpe
 set mountdir=%pedir%\mount
-set iso=%pedir%\WinPE%bits%.iso
 set windowsdir=%mountdir%\Windows
 set ghostdir="%progdir%\Symantec\Ghost"
 set viruscan=%homedrive%%homepath%\Desktop\vscl-w32-6.0.1-l
 set mytemp=%tmp%\mypedats
+
+if "%usb%"=="no" set iso=%pedir%\WinPE%bits%.iso
+if "%usb%"=="yes" set iso=%drive%
 
 rd /s /q %pedir%
 
@@ -166,7 +203,7 @@ copy *.dat %windowsdir%
 
 dism /unmount-image /mountdir:%mountdir% /commit
 
-call makewinpemedia /iso %pedir% %iso%
-copy %iso% %InitialLocation%
+call makewinpemedia %buildType% %pedir% %iso%
+if "%usb%"=="no" copy %iso% %InitialLocation%
 :end
 cd %InitialLocation%
